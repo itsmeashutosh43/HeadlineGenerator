ca5c15946a2725630d6009f60b03eb84e9d5d430
Krishuthapa <41799251+Krishuthapa@users.noreply.github.com>
2019-06-15 15:10:34 +0545

    Update scraping.py
    Data Scraper for online khabar

--- scraping.py
+++ scraping.py
@@ -1,56 +1,90 @@
-from bs4 import *
-import urllib.request
-import json,csv
-import re
+import requests
 
-urlpg='https://onlinekhabr.onlinenetwork.com/category/24'
-page = urllib.request.urlopen(urlpg)
-soup = BeautifulSoup(page, 'html.parser')
-pattern='https://onlinekhabar.onlinenetwork.com'
-article = soup.find_all('div', attrs={'class': 'col-sm-3 part-ent'})
-url=[]
-for k in article:
-    urls = list(set([a['href'] for a in k.findAll(name='a', href=True)]))
-    url.append(urls)
-article1=soup.find_all('div', attrs={'class': 'col-sm-9 detail-on'})
-url1=[]
-for k in article1:
-    urls = list(set([a['href'] for a in k.findAll(name='a', href=True)]))
-    url1.append(urls)
-finalurl=[]
-for k in url1:
-    for j in k:
-        finalurl.append(j)
-for k in url:
-    for j in k:
-        finalurl.append(j)
-for url in finalurl:
-    if re.search(pattern,url):
-        finalurl[finalurl.index(url)]=url
-    else:
-        finalurl[finalurl.index(url)]="https://onlinekhabar.onlinenetwork.com"+url
-print(finalurl)
-data={}
-print(len(finalurl))
-for url in finalurl:
-    page1 = urllib.request.urlopen(url)
-    soup1=BeautifulSoup(page1, 'html.parser')
-    headings=soup1.find(name='div',attrs={'class': 'inner-section cover-news'})
-    heading= [heading.get_text(strip=True)
+import bs4
 
-                     for heading in headings.findAll('h1')]
-    paragraphs = soup1.find(name='div', attrs={
+import pandas as pd
 
-            'class': 'tag news-content'})
-    
+import json
 
-    paragraph = [paragraph.get_text(strip=True)
+import csv
+
+
+
+
+
+def basicSoup(url):
+
+    page = requests.get(url)
+
+    soup = bs4.BeautifulSoup(page.content, 'html.parser')
+
+    return soup
+
+
+
+
+
+data = {}
+
+session = requests.Session()
+session.max_redirects=60
+
+data_to_file = open("C:/Users/Krishu Thapa/Desktop/__pycache__/onlinekhb.csv",'w',encoding='utf-16')
+
+csv_writer = csv.writer(data_to_file)
+
+'''
+
+This code only scrapes the technology news. Change myUrl to scrape other news as well.
+
+'''
+
+myUrl = "https://www.onlinekhabar.com/content/business/technology"
+
+
+
+for i in range(1, 36):
+
+    print("Doing page ", i)
+
+    url = myUrl+'/page'+str(i)
+
+    soup = basicSoup(url)
+
+
+
+    allNews = soup.find(name='div', attrs={'class': 'list_child_wrp'})
+
+    urls = list(set([a['href']
+
+                     for a in allNews.findAll(name='a',attrs={'class': 'title__regular'},href=True)]))
+
+
+
+    for url in urls:
+        
+        soup = basicSoup(url)
+
+        heading = soup.find(name='h2', attrs={'class': 'mb-0'})
+
+        heading = heading.get_text()
+
+
+
+        paragraphs = soup.find(name='div', attrs={
+
+            'class': 'ok__news--wrap'})
+
+
+
+        paragraph = [paragraph.get_text(strip=True)
 
                      for paragraph in paragraphs.findAll('p')]
-    
 
-    data["".join(heading)] = "".join(paragraph)
-data_to_file= open( "C:/Users/Krishu Thapa/Desktop/__pycache__/onlinekhabar.csv",'w',encoding='utf-16')
-csv_write=csv.writer(data_to_file)
-for row1,row2 in data.items():
-   csv_write.writerow([row1,row2])
+
+        data[heading] = paragraph
+
+
+    for row1, row2 in data.items():
+
+        csv_writer.writerow([row1, row2])
